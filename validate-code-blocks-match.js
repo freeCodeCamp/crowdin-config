27d27e6b23455302f19f0b2ef62926473c963834
Randell Dawson <rdawson@onepathtech.com>
2021-01-28 12:32:41 -0700

    feat: add existing custom qa checks

---
+++ validate-code-blocks-match.js
@@ -0,0 +1,23 @@
+// Validate Code Blocks Match
+// Validates that source and translation inline or multi-line code block strings match 100%
+
+var source = crowdin.source;
+var translation = crowdin.translation;
+var context = crowdin.context;
+
+var isCode = function(str) {
+  return /^\/pre\/code|\/code$/.test(str);
+};
+
+if (isCode(context)) {
+  if (source === translation) {
+    result.success = true;
+  } else {
+    result.message = 'Source and Translation for inline or multi-line code blocks must match. Delete the translation, so the English source is kept';
+    result.fixes = [];
+  }
+} else {
+  result.success = true;
+}
+
+return result;
